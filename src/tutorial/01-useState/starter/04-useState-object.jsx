f91a9c6f72b70b1a8941a25c2fe1a7e5a3bceadb
crossroads30 <crossroads30@mail.ru>
2023-12-27 21:39:23 +0300

    feat: add useState obj example

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,45 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+	const [person, setPerson] = useState({
+		name: 'Peter',
+		age: 27,
+		joy: 'Computers',
+	})
+	// const [name, setName] = useState('Peter')
+	// const [age, setAge] = useState(27)
+	// const [joy, setJoy] = useState('Computers')
 
-export default UseStateObject;
+	// const obj = {
+	// 	name: 'John',
+	// 	age: 23,
+	// 	joy: 'Cars',
+	// }
+	// const changeUser = () => {
+	// 	setName(obj.name)
+	// 	setAge(obj.age)
+	// 	setJoy(obj.joy)
+	// }
+	const changeUser = () => {
+		// setPerson({
+		// 	name: 'John',
+		// 	age: 23,
+		// 	joy: 'Cars',
+		// })
+    setPerson({...person, name: 'Robert'})// we have changed only the name!
+	}
+
+	return (
+		<>
+			<h2>useState object example</h2>
+			<h4>{person.name}</h4>
+			<h4>{person.age}</h4>
+			<h4>{person.joy}</h4>
+			<button onClick={changeUser} className='btn' type='button'>
+				Change person
+			</button>
+		</>
+	)
+}
+
+export default UseStateObject
