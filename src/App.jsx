2464f0bca4e6b45589a389794c2bb58e1e84d322
crossroads30 <crossroads30@mail.ru>
2023-12-28 16:53:55 +0300

    feat: add short-circuit examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data'
-import Starter from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview'
+import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples'
 
 function App() {
   return (
