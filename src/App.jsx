6533b78503ff8def5e048ebeec79942fe13c72d3
crossroads30 <crossroads30@mail.ru>
2023-12-28 17:45:22 +0300

    feat: add user challenge solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data'
-import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
+import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,32 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+	const [user, setUser] = useState(null)
+
+	const login = () => {
+		setUser({
+			name: 'James',
+		})
+	}
+	const logout = () => {
+		setUser(null)
+	}
+	return (
+		<div>
+			<h2>user challenge</h2>
+			{user ? (
+				<div>
+					<h4>hello there user {user.name}</h4>
+          <button onClick={logout} className='btn'>logout</button>
+				</div>
+			) : (
+				<div>
+					<h4>Please login</h4>
+          <button onClick={login} className='btn'>login</button>
+				</div>
+			)}
+		</div>
+	) 
+}
+
+export default UserChallenge
