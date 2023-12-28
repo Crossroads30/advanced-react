2e3c20d00c112e3deba13eddae26cf2c2355a0c5
crossroads30 <crossroads30@mail.ru>
2023-12-28 15:03:16 +0300

    feat: add solution for cond render challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
+import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data'
+import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
 
 function App() {
   return (
     <div className='container'>
       <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,49 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
-};
-export default MultipleReturnsFetchData;
+	const [user, setUser] = useState(null)
+	const [isFetching, setIsFetching] = useState(true)
+	const [isError, setIsError] = useState(false)
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const response = await fetch(url)
+				const user = await response.json()
+				// console.log(user)
+				setUser(user)
+			} catch (error) {
+				console.log(error)
+				setIsError(true)
+			}
+			setIsFetching(false)
+		}
+		setTimeout(() => {
+			fetchUser()
+		}, 4000)
+	}, [])
+
+	if (isFetching) {
+		return <h3>fetching...</h3>
+	}
+	if (isError) {
+		return <h3>There was an error!</h3>
+	}
+	return (
+		<div>
+			<h2>My Solution of Fetch Data</h2>
+			<div>
+				<img
+					src={user.avatar_url}
+					alt='user'
+					style={{ width: '150px', borderRadius: '30px' }}
+				/>
+			</div>
+			<h2>{user.name}</h2>
+			<h4>Works at {user.company}</h4>
+			<p>{user.bio}</p>
+		</div>
+	)
+}
+export default MultipleReturnsFetchData
