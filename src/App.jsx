7b1a0ebb82455c59d317b952321c134f3538cc55
crossroads30 <crossroads30@mail.ru>
2023-12-28 17:27:49 +0300

    feat: add conditional challenge solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data'
-import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples'
+import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,40 @@
-const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
-};
+import { useState } from 'react'
 
-export default ToggleChallenge;
+const ToggleChallenge = () => {
+	const [content, setContent] = useState(true)
+  
+  // First:
+	// const contentToggle = () => {
+	// 	if (content) {
+	// 		setContent(false)
+	// 		return
+	// 	}
+	// 	setContent(true)
+	// } 
+
+  //Second:
+	// const contentToggle = () => {
+	// 	content ? setContent(false) : setContent(true)
+	// }
+
+	return (
+		<div>
+			<h2>toggle challenge</h2>
+			{content ? <h2>Old Content</h2> : <Content />}
+			{/* <button onClick={contentToggle} className='btn'> */}
+			<button onClick={() => setContent(!content)} className='btn'> {/*Third*/}
+				click to change
+			</button>
+		</div>
+	)
+}
+
+const Content = () => {
+	return (
+		<div>
+			<h2>New Content</h2>
+		</div>
+	)
+}
+
+export default ToggleChallenge
