22ee6f648c0e921bb1b993877d712218c3544d44
crossroads30 <crossroads30@mail.ru>
2023-12-28 16:36:53 +0300

    feat: add short-circuit examples

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,20 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
-};
-export default ShortCircuitOverview;
+	const [falsy, setFalsy] = useState('')
+	const [truthy, setTruthy] = useState('truthy')
+
+  const codeExample = falsy || 'Hello!'
+
+    return (
+			<div>
+				<h4>Falsy OR: {falsy || 'Hello!'}</h4>
+				<h4>Falsy AND: {falsy && 'Hello!'}</h4>
+				<h4>Truthy OR: {truthy || 'Hello!'}</h4>
+				<h4>Truthy OR: {truthy && 'Hello!'}</h4>
+        {codeExample}
+			</div>
+		)
+
+}
+export default ShortCircuitOverview
