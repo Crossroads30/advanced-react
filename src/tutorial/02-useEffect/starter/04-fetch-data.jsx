193a1bf82cdc83ebb2d3501504fddc4ca2e6530f
crossroads30 <crossroads30@mail.ru>
2023-12-28 14:02:47 +0300

    feat: add useEffect with async func example

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,41 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from 'react'
+
+const url = 'https://api.github.com/users'
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
-};
-export default FetchData;
+	const [users, setUsers] = useState([])
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				const response = await fetch(url)
+				const users = await response.json()
+				setUsers(users)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchUsers()
+	}, [])
+
+	return (
+		<section>
+			<h3>fetch data example</h3>
+			<ul className='users'>
+				{users.map(user => {
+					const { id, avatar_url, login, html_url } = user
+					return (
+						<li key={id}>
+							<img src={avatar_url} alt={login} />
+							<div>
+								<h5>{login}</h5>
+								<a href={html_url}>profile</a>
+							</div>
+						</li>
+					)
+				})}
+			</ul>
+		</section>
+	)
+}
+export default FetchData
