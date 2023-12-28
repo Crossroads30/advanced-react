de76977f9c931b477402178dc3c27ca5ae8ff968
crossroads30 <crossroads30@mail.ru>
2023-12-28 11:54:14 +0300

    feat: add useState with prev state example

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,31 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+	const [value, setValue] = useState(0)
+
+	// const increment = () => {
+	// 	setValue(currentState => {
+	// 		const newState = currentState + 1
+	// 		return newState
+	// 	})
+  //   console.log(value)
+	// }
+	const increment = () => {
+    setTimeout(() => {
+      setValue((prev) => prev + 1) //we pass the previous state as the papam to the func inside 'setWhatever' of useState function & then return this new state(if this not in one line, we must use 'return' keyword!!!)
+    }, 3000)
+	}
+	console.log(value)
+
+
+	return (
+		<>
+			<h1>{value}</h1>
+			<button onClick={increment} type='button' className='btn'>
+				increase
+			</button>
+		</>
+	)
+}
+
+export default UseStateGotcha
