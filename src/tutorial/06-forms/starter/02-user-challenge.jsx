c1fd51667570a302531a8049dc11ff6a530c0f0f
crossroads30 <crossroads30@mail.ru>
2023-12-29 11:24:40 +0300

    feat: add addUser challenge solution

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,62 @@
-const UserChallenge = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input type='text' className='form-input' id='name' />
-        </div>
+import { useState } from 'react'
+import { data } from '../../../data'
 
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-      {/* render users below */}
-    </div>
-  );
-};
-export default UserChallenge;
+const UserChallenge = () => {
+	const [nameValue, setNameValue] = useState('')
+	const [users, setUsers] = useState(data)
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		// console.log(nameValue)
+		if (!nameValue) {
+			alert('Please Enter the Name!')
+			return
+		}
+		const newUser = { id: users.length + 1, name: nameValue }
+		setUsers([...users, newUser])
+		setNameValue('')
+	}
+	// console.log(users)
+
+	return (
+		<div>
+			<form className='form' onSubmit={handleSubmit}>
+				<h4>Add User</h4>
+				<div className='form-row'>
+					<label htmlFor='name' className='form-label'>
+						name
+					</label>
+					<input
+						type='text'
+						className='form-input'
+						id='name'
+						value={nameValue}
+						onChange={e => setNameValue(e.target.value)}
+					/>
+				</div>
+
+				<button type='submit' className='btn btn-block'>
+					submit
+				</button>
+			</form>
+			{/* render users below */}
+			<div>
+				<h2>Users</h2>
+				<ul className='users' style={{ display: 'block', width: '37rem' }}>
+					{users.map(user => {
+						const { id, name } = user
+						return (
+							<li
+								key={id}
+								style={{ marginBottom: '1rem', justifyContent: 'center' }}
+							>
+								<h3>{name}</h3>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+		</div>
+	)
+}
+export default UserChallenge
