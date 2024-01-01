442fe20f688b3eddfa99020e81e13e0b6f845006
crossroads30 <crossroads30@mail.ru>
2024-01-01 23:04:22 +0300

    feat: add avoiding rendering with component mount ex

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -1,36 +1,56 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
 
 const UseRefBasics = () => {
-  const [value, setValue] = useState(0);
+	const [value, setValue] = useState(0)
+	const refContainer = useRef(null)
+	const isMounted = useRef(false)
+	// console.log(refContainer)
 
-  const refContainer = useRef(null)
-  console.log(refContainer)
+	useEffect(() => {
+		// we can use useRef to focus the input
+		refContainer.current.focus()
+	})
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const type = refContainer.current.value
-    console.log(type)
-  };
+	//to use this solution let avoid running functionality that inside with initial render
+	useEffect(() => {
+		if (!isMounted.current) {
+			// we should check not 'isMounted'(this is the object), but isMounted.current!!!(this is the value!!!)
+			isMounted.current = true
+			return
+		}
+		console.log('rerender')
+	}, [value])
 
-  return (
-    <div>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            Name
-          </label>
-          <input type='text' id='name' className='form-input' ref={refContainer} />
-        </div>
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-      <h1>value : {value}</h1>
-      <button onClick={() => setValue(value + 1)} className='btn'>
-        increase
-      </button>
-    </div>
-  );
-};
+	const handleSubmit = e => {
+		e.preventDefault()
+		const type = refContainer.current.value
+		// console.log(type)
+	}
 
-export default UseRefBasics;
+	return (
+		<div>
+			<form className='form' onSubmit={handleSubmit}>
+				<div className='form-row'>
+					<label htmlFor='name' className='form-label'>
+						Name
+					</label>
+					<input
+						type='text'
+						id='name'
+						className='form-input'
+						ref={refContainer}
+					/>
+				</div>
+				<button type='submit' className='btn btn-block'>
+					submit
+				</button>
+			</form>
+			<h1>value : {value}</h1>
+			<button onClick={() => setValue(value + 1)} className='btn'>
+				increase
+			</button>
+		</div>
+	)
+}
+
+export default UseRefBasics
