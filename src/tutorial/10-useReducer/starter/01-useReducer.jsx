71e1d8270fdca06d41802be06f4866a8281fafcf
crossroads30 <crossroads30@mail.ru>
2024-01-05 12:27:55 +0300

    feat: add solution for reset data

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,32 +1,54 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from 'react'
+import { data } from '../../../data'
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+	const [people, setPeople] = React.useState(data)
+	const [isPeople, setIsPeople] = React.useState(true)
 
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
-  return (
-    <div>
-      {people.map((person) => {
-        const { id, name } = person;
-        return (
-          <div key={id} className='item'>
-            <h4>{name}</h4>
-            <button onClick={() => removeItem(id)}>remove</button>
-          </div>
-        );
-      })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
-    </div>
-  );
-};
+	const removeItem = id => {
+		let newPeople = people.filter(person => person.id !== id)
+		setPeople(newPeople)
+	}
 
-export default ReducerBasics;
+	const resetItem = () => {
+		setPeople(data)
+	}
+
+	const clearPeople = () => {
+		setIsPeople(false)
+		setPeople([])
+	}
+
+	return (
+		<div>
+			{people.map(person => {
+				const { id, name } = person
+				return (
+					<div key={id} className='item'>
+						<h4>{name}</h4>
+						<button onClick={() => removeItem(id)}>remove</button>
+					</div>
+				)
+			})}
+			{!isPeople && (
+				<button
+					className='btn'
+					style={{ marginTop: '2rem' }}
+					onClick={resetItem}
+				>
+					reset items
+				</button>
+			)}
+			{isPeople && (
+				<button
+					className='btn'
+					style={{ marginTop: '2rem' }}
+					onClick={clearPeople}
+				>
+					clear items
+				</button>
+			)}
+		</div>
+	)
+}
+
+export default ReducerBasics
