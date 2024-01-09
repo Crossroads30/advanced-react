1f5fe1b3f9510750952e34bb84a97ce3ac754a78
crossroads30 <crossroads30@mail.ru>
2024-01-09 20:47:30 +0300

    feat: add ex of useMemo with slow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/11-performance/starter/03-hooks/FetchDataWithUseCallback'
+import Starter from './tutorial/11-performance/starter/03-hooks/index'
 
 
 function App() {
